3657db9c5df15257369de2bf09dc588518123fcf
tommy <miyuki.t.web@gmail.com>
2025-09-05 22:05:31 +0900

    5章まで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,47 +1,10 @@
 import Image from "next/image";
+import { getMembersList } from "@/app/_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
 import styles from "./page.module.css";
 
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッド・チャン",
-      position: "CEO",
-      profile:
-        "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功。",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member2.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "エミリー・サンダース",
-      position: "COO",
-      profile:
-        "テクノロジーとビジネスの交差点で活躍するリーダー。AIと機械学習の専門家であり、革新的なソリューションを通じて企業の成長を推進。",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member3.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "ジョン・ウィルソン",
-      position: "CTO",
-      profile:
-        "財務戦略とリスク管理の専門家。大手企業での経験を活かし、持続可能な成長を実現するための財務戦略を推進。",
-    },
-  ],
-};
-export default function Page() {
+export default async function Page() {
+  const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,16 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsLlist from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
 
-const data: {
-  contents: News[];
-} = {
-  contents: [
-    {
-      id: "1",
-      title: "渋谷オフィスを移転しました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/29",
-    },
-    {
-      id: "2",
-      title: "当社CEOが業界誌のインタビューを受けました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/29",
-    },
-    {
-      id: "3",
-      title: "新しいプロダクトをリリースしました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/29",
-    },
-  ],
-};
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
 
-export default function Home() {
-  const sliceData = data.contents.slice(0, 2);
   return (
     <>
       <section className={styles.top}>
@@ -60,7 +30,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsLlist news={sliceData} />
+        <NewsLlist news={data.contents} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっと見る</ButtonLink>
         </div>
